703c08404bdeaf8889820e247dfb5caec609373d
AnnA <AnnA@8.10.20.172.rev.sfr.net>
2017-09-19 16:04:51 +0200

    version avec la page d'acceuile

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
                 
                     <article>
                         <header>
-                            <h1 class="titreBillet"><?= $billet['titre'] ?></h1>
+                             <h1 class="titreBillet"><?= $billet['titre'] ?></h1>
                             <time><?= $billet['date'] ?></time>
                         </header>
                         <p><?= $billet['contenu'] ?></p>
