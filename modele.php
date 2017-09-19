c9ce720869cb3e056cb384231ed11c79671e80ed
AnnA <AnnA@8.10.20.172.rev.sfr.net>
2017-09-19 16:13:23 +0200

    modif avec modele

---
+++ modele.php
@@ -0,0 +1,18 @@
+<?php
+
+//acces aux données
+
+function getBillets(){
+    
+    $bdd = new PDO('mysql:host=localhost;dbname=bddBlog;charset=utf8',
+        'root', 'root');
+    $billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
+        . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
+        . ' order by BIL_ID desc');
+    
+    return $billets;
+}
+
+
+
+?>
