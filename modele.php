e495be0bb6c656cd22be87bbe816939d998a6d4b
AnnA <AnnA@8.10.20.172.rev.sfr.net>
2017-09-19 16:37:15 +0200

    SEPARATION fonction getbillet get bdd

--- modele.php
+++ modele.php
@@ -4,8 +4,7 @@
 
 function getBillets(){
     
-    $bdd = new PDO('mysql:host=localhost;dbname=bddBlog;charset=utf8',
-        'root', 'root');
+   
     $billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
         . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
         . ' order by BIL_ID desc');
@@ -13,6 +12,11 @@ function getBillets(){
     return $billets;
 }
 
-
+function getBdd(){
+    $bdd = new PDO('mysql:host=localhost;dbname=bddBlog;charset=utf8',
+        'root', 'root');
+    return  $bdd;
+    
+}
 
 ?>
