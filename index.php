be9c158f9038eba83b19ad2e090d0e2540119fb1
AnnA <AnnA@8.10.20.172.rev.sfr.net>
2017-09-19 14:35:57 +0200

    Version organiser 1 de index

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+
+<?php
+//acces aux données
+                $bdd = new PDO('mysql:host=localhost;dbname=bddBlog;charset=utf8',
+                        'root', 'root');
+                $billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
+                        . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
+                        . ' order by BIL_ID desc');
+                foreach ($billets as $billet):
+                    ?>
+
+<!-- AFFICHAGE -->
 <!doctype html>
 <html lang="fr">
     <head>
@@ -12,14 +24,7 @@
                 <p>Je vous souhaite la bienvenue sur ce modeste blog.</p>
             </header>
             <div id="contenu">
-                <?php
-                $bdd = new PDO('mysql:host=localhost;dbname=bddBlog;charset=utf8',
-                        'root', 'root');
-                $billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
-                        . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
-                        . ' order by BIL_ID desc');
-                foreach ($billets as $billet):
-                    ?>
+                
                     <article>
                         <header>
                             <h1 class="titreBillet"><?= $billet['titre'] ?></h1>
